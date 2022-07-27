58f7a0f66dc3b01b8f5044ae9f8f9b9ad35c2527
Yassine <yassinoubou93@gmail.com>
2022-07-27 18:51:21 +0200

    first commit

---
+++ components/layout/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import Colors from "../../constants/colors";
+
+const Header = (props) => {
+  return <View style={StyleSheet.view}></View>;
+};
+const Styles = StyleSheet.create({
+  view: {
+    backgroundColor: Colors.Primary,
+  },
+});
+export default Header;
---
+++ constants/colors.js
@@ -0,0 +1,4 @@
+export default {
+  Primary: "#002366",
+  Text: "#212529",
+};
