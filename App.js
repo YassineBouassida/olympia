3960fbc8861f1549ef82f9cd44f17e4185b7235d
YassineBouassida <yassinoubou93@gmail.com>
2022-07-28 02:26:18 +0100

    changes 0.0.1

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import  React from "react";
+import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, Dimensions } from "react-native";
+import Header from "./components/layout/Header";
 
 export default () => {
   return (
-    <SafeAreaView>
-     
+    <SafeAreaView style={{ width: Dimensions.get("window").width }}>
+      <Header></Header>
     </SafeAreaView>
   );
 };
--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,13 +1,39 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Image, Dimensions } from "react-native";
 import Colors from "../../constants/colors";
 
 const Header = (props) => {
-  return <View style={StyleSheet.view}></View>;
+  return (
+    <View style={{ ...Styles.view, ...props.style }}>
+      <View style={Styles.brand}>
+        <Image
+          style={Styles.logo}
+          source={require("../../assets/img/logo.png")}
+        />
+        <Image
+          style={Styles.logo}
+          source={require("../../assets/img/logo_type.png")}
+        />
+      </View>
+    </View>
+  );
 };
 const Styles = StyleSheet.create({
   view: {
     backgroundColor: Colors.Primary,
+    alignItems: "center",
+    height: 90,
+    width: "100%",
+    justifyContent: "space-between",
+  },
+  brand: {
+    maxWidth: Dimensions.get("window").width / 3,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  logo: {
+    height: 80,
+    width: 80,
   },
 });
 export default Header;
